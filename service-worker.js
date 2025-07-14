59fe0b3bf71a27ce5627cc2855e26ba3a46872d6
aijoe/john <johnrward723@gmail.com>
2025-07-14 14:34:15 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,1 @@
-// Simple service worker to enable PWA install
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>self.clients.claim());
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
