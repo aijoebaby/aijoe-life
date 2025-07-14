caecc1d8eb9fc83920423d9bab3c7e4f47215612
aijoe/john <johnrward723@gmail.com>
2025-07-14 11:10:11 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('aijoe').then(c=>c.addAll(['index.html','manifest.json','joey-avatar.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>e.waitUntil(caches.open('aijoe').then(c=>c.addAll(['index.html','manifest.json','joey-avatar.png','joey-bg.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
