7e71dff64420a53b9762ad5902c2ea2f90f75b65
aijoe/john <johnrward723@gmail.com>
2025-07-14 11:49:26 -0700

    Delete AIJOE_LIFE_BIBLE_READY directory

--- AIJOE_LIFE_BIBLE_READY/service-worker.js
+++
@@ -1,3 +0,0 @@
-
-self.addEventListener('install',e=>{e.waitUntil(caches.open('aijoe').then(c=>c.addAll(['index.html','manifest.json','joey-avatar.png'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
